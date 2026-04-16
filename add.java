0be74bd019f1cc3971ff3c5c42f59a77a280f847
Jhansi-t21 <jhansireddy025@gmail.com>
2026-04-16 23:24:58 +0530

    Create add.java

---
+++ add.java
@@ -0,0 +1,9 @@
+public class SimpleAdd {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+
+        System.out.println("Addition of " + a + " and " + b + " is: " + sum);
+    }
+}
